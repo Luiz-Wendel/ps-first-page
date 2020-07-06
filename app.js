4127c14310b43bdaab8f352f5fdc7e535acb9b10
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-07-06 15:18:53 -0300

    Adding eslint

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const chalk = require('chalk')
-const debug = require('debug')('app')
-const morgan = require('morgan')
-const path = require('path')
+const express = require('express');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 // Print requests on terminal
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 
-app.use(express.static(path.join(__dirname, 'node_modules')))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 app.listen(3002, () => {
-  console.log(`Listening on port ${chalk.green('3002')}...`)
-})
+  console.log(`Listening on port ${chalk.green('3002')}...`);
+});
