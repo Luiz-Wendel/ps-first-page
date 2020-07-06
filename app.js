4d51633e294e74bb33a4105a4c0ca967e57a02c5
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-07-06 12:10:10 -0300

    Showing html file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const chalk = require('chalk')
 const debug = require('debug')('app')
 const morgan = require('morgan')
+const path = require('path')
 
 const app = express()
 
@@ -9,7 +10,7 @@ const app = express()
 app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
-  res.send('Hello from my project!')
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 app.listen(3002, () => {
