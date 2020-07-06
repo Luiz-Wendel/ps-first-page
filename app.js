4365b736937d6c0ed663849a517a0c90a5370fad
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-07-06 12:01:08 -0300

    Debug and morgan setup

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const chalk = require('chalk')
+const debug = require('debug')('app')
+const morgan = require('morgan')
 
 const app = express()
+
+// Print requests on terminal
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('Hello from my project!')
 })
 
 app.listen(3002, () => {
-  console.log('Listening on port 3002...')
+  console.log(`Listening on port ${chalk.green('3002')}...`)
 })
